7190e5e3a22a8aabe0108d6023f09bc0d71dab5f
321hu <2665676896@qq.com>
2023-10-05 11:35:17 +0800

    update

---
+++ NAcode/header.h
@@ -0,0 +1,6 @@
+#ifndef _HEADER_HEAD_
+#define _HEADER_HEAD_
+
+void hu();
+
+#endif
---
+++ NAcode/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "header.h"
+using namespace std;
+
+int main() {
+    hu();
+    return 0;
+}
